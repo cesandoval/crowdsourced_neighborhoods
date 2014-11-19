67d08209d82ac38f25004840fe39112f058af214
cesandoval <ce.sandoval09@gmail.com>
2014-11-19 12:29:09 -0500

    read instagram

--- python/read_instagram.py
+++ python/read_instagram.py
@@ -9,50 +9,48 @@ insta_jsons = [ f for f in listdir(insta_path) if isfile(join(insta_path,f)) ]
 csv_file = open('csv/instagram_csv.csv', 'wt')
 
 writer = csv.writer(csv_file)
-writer.writerow(['lat', 'lon', 'time', 'content', 'tags'])
+writer.writerow(['pid', 'lat', 'lon', 'time', 'content', 'tags'])
 
-cnt = 0
+
 all_ids = []
-for f in [insta_jsons[0]]:
+for f in insta_jsons:
     with open('instagram/%s' %(f)) as json_file:
         grams = json.load(json_file)
-        
-        # every instagram json has a number of lists with a dictionary that has a data attribute which is a list of pics on its own
-        #print json.dumps(grams[3]['data'][0])
-        #print len(grams)
-        
+        cnt = 0
         for gram in grams:
             if gram != None:
                 if len(gram['data']) != 0:
-                    print gram['data']
-                    # check if the id of the gram is in all_ids....
-            #content = gram['content']
-            
-            '''
-            p = ttp.Parser()
-            parsed_gram = p.parse(content)
-            hashtags = [tag.encode('utf-8') for tag in parsed_gram.tags]
-            if float(gram['lat']) != 0 and float(gram['lon']) != 0:
-                writer.writerow([gram['lat'], gram['lon'], gram['time'], gram['content'].encode('utf-8'), str(hashtags)])
-                cnt += 1'''
-            
-
-print cnt
+                    for data in gram['data']:
+                        #image, user, content, time, iid, lat, lon = '', '', '', '', '', '', ''
+                        content, image, user, caption, time, iid, lat, lon = '', None, None, None, None, None, None, None
+                        try: image = data['images']['standard_resolution']['url']
+                        except: pass
+                        try: user = data['user']['id']
+                        except: pass
+                        try: content = data['caption']['text']
+                        except: pass
+                        try: time = data['caption']['created_time']
+                        except: pass
+                        try: iid = data['id']
+                        except: pass
+                        try: lat = data['location']['latitude']
+                        except: pass
+                        try: lon = data['location']['longitude']
+                        except: pass
+                        raw_source = data
+                        
+                        if lat != None and lon != None:
+                            if iid not in all_ids:
+                                p = ttp.Parser()
+                                hashtags = None
+                                parsed_gram = p.parse(content)
+                                hashtags = [tag.encode('utf-8') for tag in parsed_gram.tags]
+                                #print hashtags
+                                writer.writerow([iid, lat, lon, time, content.encode('utf-8'), str(hashtags)])
+                                
+                                all_ids.append(iid)
+                                cnt += 1                        
+                        
+    print "Added %d new grams, for a total of %d" % (cnt, len(all_ids))                        
 
 csv_file.close()
-
-'''
-type of data we have..... gis, social media
-
-what are we trying to get from tweets?
-what characterizes a neighborhood?
-4sq
--venue types
-
-twitter
--how twitter is used:
-    -times
-    -density
-    -length of tweets
-    - how to find activities that are unique to a particular location?
-'''
--- python/read_twitter.py
+++ python/read_twitter.py
@@ -12,6 +12,7 @@ writer = csv.writer(csv_file)
 writer.writerow(['lat', 'lon', 'time', 'content', 'tags'])
 
 cnt = 0
+all_ids = []
 for f in twi_jsons:
     with open('twitter/%s' %(f)) as json_file:
         tweets = json.load(json_file).values()
@@ -21,11 +22,10 @@ for f in twi_jsons:
             parsed_tweet = p.parse(content)
             hashtags = [tag.encode('utf-8') for tag in parsed_tweet.tags]
             if float(tweet['lat']) != 0 and float(tweet['lon']) != 0:
-                writer.writerow([tweet['lat'], tweet['lon'], tweet['time'], tweet['content'].encode('utf-8'), str(hashtags)])
-                cnt += 1
-            
+                if tweet['tweet_id'] not in all_ids:
+                    writer.writerow([tweet['lat'], tweet['lon'], tweet['time'], tweet['content'].encode('utf-8'), str(hashtags)])
+                    cnt += 1
 
 print cnt
-
 csv_file.close()
 
